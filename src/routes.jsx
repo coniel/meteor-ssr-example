b60721cca77d74b63e6ead582f8010aca2b3cfae
tomatagolo <tom@agolo.com>
2016-10-18 17:03:50 -0400

    modify to add material-ui

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,45 @@
-import { Route } from 'react-router';
+import { Link, Route, browserHistory } from 'react-router';
+import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles';
+
+import { RaisedButton } from 'material-ui';
 import { ReactRouterSSR } from 'meteor/reactrouter:react-router-ssr';
 import todoRoutes from 'TodoApp/client/routes';
 
+const About = () => (
+  <div>
+    <h1>ABOUT</h1>
+    <RaisedButton
+      label='HOME'
+      onTouchTap={() => browserHistory.push('/')}
+    />
+    <Link to='/privacy'>privacy</Link>
+  </div>
+);
+
+const Privacy = () => (
+  <div>
+    <h1>PRIVACY</h1>
+    <RaisedButton
+      label='HOME'
+      onTouchTap={(e) => {
+        e.preventDefault();
+        browserHistory.push('/')
+      }}
+    />
+    <Link to='/about'>about</Link>
+  </div>
+);
+
+const AppContainer = ({children}) => (
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    {children}
+  </MuiThemeProvider>
+);
+
 ReactRouterSSR.Run(
-  <Route>
+  <Route component={AppContainer}>
     {todoRoutes}
+    <Route component={About} path='/about' />
+    <Route component={Privacy} path='/privacy' />
   </Route>
 );
